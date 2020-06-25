03590fe80123a803461f3f3d1b01231e5a21ad5f
Divyanshu Raj <divyanshuraj5050@gmail.com>
2020-06-25 23:58:08 +0530

    fixed the bootsrap problem using dyanmic insertion and removal of link tag containing bootrap  cdn

--- src/Contents/admin/Admin.js
+++ src/Contents/admin/Admin.js
@@ -52,6 +52,13 @@ const useStyles = makeStyles((theme) => ({
 
 export default function AdminSignIn() {
   const classes = useStyles();
+  componentDidMount(){
+    document.head.innerHTML+= '<link id ="bootstrap " href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">' ;
+}
+componentWillUnmount(){
+
+    document.querySelector("#bootstrap\\ ").remove();
+}
 
   return (
     <Container component="main" maxWidth="xs">
