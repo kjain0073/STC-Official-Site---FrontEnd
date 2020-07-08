870fd1f28d19f838903d352fce22d4ecd8200004
kjain0073 <kjain20040073@gmail.com>
2020-07-09 00:01:16 +0530

    Updates

---
+++ precache-manifest.d6e19c12997e27ec596f2226afabfaac.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4578936a132001d5bc11bbac303b56fb",
+    "url": "/STC-Official-Site---FrontEnd/index.html"
+  },
+  {
+    "revision": "077e8815c960f2372c84",
+    "url": "/STC-Official-Site---FrontEnd/static/css/2.459abc08.chunk.css"
+  },
+  {
+    "revision": "c467651d792e561762d0",
+    "url": "/STC-Official-Site---FrontEnd/static/css/main.3a736415.chunk.css"
+  },
+  {
+    "revision": "077e8815c960f2372c84",
+    "url": "/STC-Official-Site---FrontEnd/static/js/2.01d030de.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/STC-Official-Site---FrontEnd/static/js/2.01d030de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c467651d792e561762d0",
+    "url": "/STC-Official-Site---FrontEnd/static/js/main.bd20439c.chunk.js"
+  },
+  {
+    "revision": "8e2c256d7ce2aef7fb5f",
+    "url": "/STC-Official-Site---FrontEnd/static/js/runtime-main.88d4d992.js"
+  },
+  {
+    "revision": "2224dfbe4bb3f6dfa7af215f884b52de",
+    "url": "/STC-Official-Site---FrontEnd/static/media/Notice.2224dfbe.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/STC-Official-Site---FrontEnd/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/STC-Official-Site---FrontEnd/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/STC-Official-Site---FrontEnd/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/STC-Official-Site---FrontEnd/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/STC-Official-Site---FrontEnd/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
